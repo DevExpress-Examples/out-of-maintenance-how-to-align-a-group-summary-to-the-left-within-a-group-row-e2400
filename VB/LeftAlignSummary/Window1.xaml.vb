31fd2ede236031ba7c303d9197348cf2acc19db0
DevExpressExampleBot <fakeEmail>
2022-12-16 18:03:47 +0400

    Source auto update [skip ci]

--- VB/LeftAlignSummary/Window1.xaml.vb
+++ VB/LeftAlignSummary/Window1.xaml.vb
@@ -1,71 +1,46 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
 Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
-Imports System.Collections
 Imports DevExpress.Data
 Imports DevExpress.Xpf.Grid
 
 Namespace LeftAlignSummary
-	''' <summary>
-	''' Interaction logic for Window1.xaml
-	''' </summary>
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			Dim list As New List(Of Record)()
-			list.Add(New Record() With {.GroupName = "Group A", .Value = 19})
-			list.Add(New Record() With {.GroupName = "Group B", .Value = 30})
-			list.Add(New Record() With {.GroupName = "Group A", .Value = 15})
-			list.Add(New Record() With {.GroupName = "Group C", .Value = 42})
-			gridControl1.ItemsSource = list
-			gridControl1.Columns("GroupName").GroupIndex = 0
-		End Sub
-	End Class
 
-	Public NotInheritable Class HiddenSummaryTemplateSelector
-		Inherits DataTemplateSelector
-		Private Shared ReadOnly EmptyTemplate As New DataTemplate()
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Public Partial Class Window1
+        Inherits Window
 
-		Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
-			Dim data As GridGroupSummaryData = TryCast(item, GridGroupSummaryData)
-			If data.SummaryItem.SummaryType = SummaryItemType.Count Then
-				Return EmptyTemplate
-			End If
+        Public Sub New()
+            Me.InitializeComponent()
+            Dim list As List(Of Record) = New List(Of Record)()
+            list.Add(New Record() With {.GroupName = "Group A", .Value = 19})
+            list.Add(New Record() With {.GroupName = "Group B", .Value = 30})
+            list.Add(New Record() With {.GroupName = "Group A", .Value = 15})
+            list.Add(New Record() With {.GroupName = "Group C", .Value = 42})
+            Me.gridControl1.ItemsSource = list
+            Me.gridControl1.Columns("GroupName").GroupIndex = 0
+        End Sub
+    End Class
 
-			Return MyBase.SelectTemplate(item, container)
-		End Function
-	End Class
+    Public NotInheritable Class HiddenSummaryTemplateSelector
+        Inherits DataTemplateSelector
 
-	Public Class Record
-		Private privateGroupName As String
-		Public Property GroupName() As String
-			Get
-				Return privateGroupName
-			End Get
-			Set(ByVal value As String)
-				privateGroupName = value
-			End Set
-		End Property
-		Private privateValue As Integer
-		Public Property Value() As Integer
-			Get
-				Return privateValue
-			End Get
-			Set(ByVal value As Integer)
-				privateValue = value
-			End Set
-		End Property
-	End Class
+        Private Shared ReadOnly EmptyTemplate As DataTemplate = New DataTemplate()
+
+        Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
+            Dim data As GridGroupSummaryData = TryCast(item, GridGroupSummaryData)
+            If data.SummaryItem.SummaryType = SummaryItemType.Count Then Return EmptyTemplate
+            Return MyBase.SelectTemplate(item, container)
+        End Function
+    End Class
+
+    Public Class Record
+
+        Public Property GroupName As String
+
+        Public Property Value As Integer
+    End Class
 End Namespace
